23625ef3426ac8f7efff0ca3c8edab69d920dc78
Dang Van Dau <dang.van.dau@bpotech.com.vn>
2021-07-15 09:13:46 +0700

    chart report

--- app/Http/Controllers/Api/SummaryController.php
+++ app/Http/Controllers/Api/SummaryController.php
@@ -6,32 +6,55 @@ use App\Http\Controllers\Controller;
 use App\Models\Customer;
 use App\Models\Job;
 use Carbon\Carbon;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class SummaryController extends Controller
 {
-    public function chartReport()
+    public function chartReport(Request $request)
     {
         $now = Carbon::now();
-        $months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-        $saleRevenues = Job::whereYear('StartDate', $now->year)
-            ->selectRaw('sum("Price") as revenue, extract(month from "StartDate") as month') // month(StartDate) //extract(month from StartDate)
-            ->groupBy(DB::raw('extract(month from "StartDate")')) //month(StartDate) //extract(month from StartDate)
+        try {
+            $from = $request->get('from') ? Carbon::createFromFormat('Y-m-d', $request->get('from')) : Carbon::create($now->year, 1, 1);
+            $to = $request->get('to') ? Carbon::createFromFormat('Y-m-d', $request->get('to')) : $now;
+        } catch (\Exception $ex) {
+            $from = Carbon::create($now->year, 1, 1);
+            $to = $now;
+        }
+        $labels = [];
+
+        $label = $from->copy();
+
+        while ($label < $to) {
+            $labels[] = $label->format('Y-m');
+            $label = $label->addMonth(1);
+        }
+
+        $saleRevenues = Job::whereBetween('StartDate', [$from, $to])
+            ->selectRaw('sum("Price") as revenue, to_char("StartDate", \'YYYY-MM\') as month') // ->selectRaw('sum(Price) as revenue, date_format(StartDate, "%Y-%m") as month')
+            ->groupBy(DB::raw('to_char("StartDate", \'YYYY-MM\')')) //->groupBy(DB::raw('date_format(StartDate, "%Y-%m")'))
+            ->get();
+
+        $paymentRevenues = Job::whereBetween('Paydate', [$from, $to])
+            ->where('Paid', true)
+            ->selectRaw('sum("Price") as revenue, to_char("Paydate", \'YYYY-MM\') as month') // ->selectRaw('sum(Price) as revenue, date_format(Paydate, "%Y-%m") as month')
+            ->groupBy(DB::raw('to_char("Paydate", \'YYYY-MM\')')) //->groupBy(DB::raw('date_format(Paydate, "%Y-%m")'))
             ->get();
 
         $data = [];
+        $data['months'] = $labels;
 
-        foreach ($months as $index => $name) {
-            if ($saleRevenues->contains('month', $index + 1)) {
-                array_push($data, [
-                    'month' => $name,
-                    'revenue' => $saleRevenues->firstWhere('month', $index + 1)->revenue,
-                ]);
+        foreach ($labels as $name) {
+            if ($saleRevenues->contains('month', $name)) {
+                $data['sales'][] = $saleRevenues->firstWhere('month', $name)->revenue;
             } else {
-                array_push($data, [
-                    'month' => $name,
-                    'revenue' => 0,
-                ]);
+                $data['sales'][] = 0;
+            }
+
+            if ($paymentRevenues->contains('month', $name)) {
+                $data['payments'][] = $paymentRevenues->firstWhere('month', $name)->revenue;
+            } else {
+                $data['payments'][] = 0;
             }
         }
 
